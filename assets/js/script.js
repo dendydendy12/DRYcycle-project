77b320d69000628001960131ca0e117002126829
yudi-git1 <yudiwahyudixyz859@gmail.com>
2025-10-06 19:01:25 +0700

    halo

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,13 @@
             preloader.style.display = "none";
         }, 600); // sesuai transition 0.5s
     });
+
+       window.addEventListener("load1", function() {
+        const preloader = document.getElementById("preloader1");
+        preloader.classList.add("fade-out");
+        
+        // setelah animasi selesai, bisa sekalian hapus dari DOM biar ringan
+        setTimeout(() => {
+            preloader.style.display = "none";
+        }, 600); // sesuai transition 0.5s
+    });
